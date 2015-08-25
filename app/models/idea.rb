8d812df4d0798fe27fa50d3cd6910c7626dcdc21
Max Tedford <max.tedford@gmail.com>
2015-08-25 13:54:15 -0600

    update idea model validation

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,7 @@
 class Idea < ActiveRecord::Base
   enum quality: %w(swill plausible genius)
   
-  validates title,   presence: true
-  validates body,    presence: true
-  validates quality, presence: true
+  validates :title,   presence: true
+  validates :body,    presence: true
+  validates :quality, presence: true
 end
