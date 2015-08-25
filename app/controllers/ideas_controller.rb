15a6a09b64ab3700de3d98a261608ec8fe74d334
Max Tedford <max.tedford@gmail.com>
2015-08-25 14:44:18 -0600

    add idea creation to index page

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,27 @@
 class IdeasController < ApplicationController
+  before_action :all_ideas, only: [:index, :create]
+  respond_to :html, :js
   
   def index
+    @idea = Idea.new
+  end
+  
+  def create
+    idea = Idea.new(idea_params)
+    if idea.save
+      flash[:notice] = "idea created!"
+    else
+      flash[:error] = errors.full_messages.join(", ")
+    end
+  end
+  
+  private
+  
+  def idea_params
+    params.require(:idea).permit(:title, :body, :quality)
+  end
+  
+  def all_ideas
+    @ideas = Idea.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :ideas, only: [:create]
+  
   root 'ideas#index'
 end
