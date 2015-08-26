ec7ef86bccec2b7416bd8b07f91cb4339ea604fa
Max Tedford <max.tedford@gmail.com>
2015-08-25 19:35:24 -0600

    remove node_modules directory and precompile for heroku push

--- app/assets/javascripts/ideas_index.js
+++ app/assets/javascripts/ideas_index.js
@@ -15,18 +15,14 @@ $(document).ready(function() {
     event.preventDefault();
     console.log("fisst");
     var title = document.getElementById('idea-title');
-    console.log("second");
     var body = document.getElementById('idea-body');
-    console.log("third");
-    $.ajax({
-      method: "POST",
-      url: "/ideas.json",
-      data: { title: title, body: body }
-    })
-      .done(function( msg ) {
-        alert( "Data Saved: " + msg );
-      });
-    console.log("fourth");
+    $.post("/ideas.json", {idea: {title: title, body: body}}).then(function(idea) {
+      $('#idea-list').prepend('<li class="list-group-item" id="' + idea.id + 
+      '"><h3 class="idea-title">' + 
+      title.value + '</h3><br><p class="idea-body">' + 
+      body.value + '</p><br><p class="idea-quality">Quality: ' + idea.quality + 
+      '</p></li>');
+    });
     $('#idea-title').val("");
     $('#idea-body').val("");
     ideaList.truncateBodies();
